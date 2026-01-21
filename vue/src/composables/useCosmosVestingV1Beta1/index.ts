e149f655f698a8b7fbb566b947c429ceb4c71087
Maria Brandão <162699125+MariaBrandao18@users.noreply.github.com>
2026-01-21 15:26:28 -0300

    Delete vue directory

--- vue/src/composables/useCosmosVestingV1Beta1/index.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useQuery, type UseQueryOptions, useInfiniteQuery, type UseInfiniteQueryOptions, type InfiniteData  } from "@tanstack/vue-query";
-import { useClient } from '../useClient';
-
-export default function useCosmosVestingV_1Beta_1() {
-  const client = useClient();
-  return {
-  }
-}
